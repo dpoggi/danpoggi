27851b7145909d6a08649b87ad1f65c19e096bf4
Dan Poggi <dan.poggi@detroitlabs.com>
2016-02-01 21:13:08 -0500

    Dynamically generate resume PDF

---
+++ lib/latex_filter.rb
@@ -0,0 +1,25 @@
+require 'fileutils'
+require 'tmpdir'
+
+class LatexFilter < Nanoc::Filter
+  identifier :latex
+  type :binary
+
+  def run(filename, params = {})
+    output_withoutext = File.basename(output_filename).sub(/#{Regexp.escape(File.extname(output_filename))}$/, '')
+
+    Dir.mktmpdir do |dir|
+      input_path = File.join(dir, "#{output_withoutext}.tex")
+      FileUtils.cp(filename, input_path)
+      output_path = File.join(dir, "#{output_withoutext}.pdf")
+
+      Dir.chdir(dir) do
+        system('pdflatex', input_path)
+        unless $? == 0
+          raise 'pdflatex failed!'
+        end
+        FileUtils.cp(output_path, output_filename)
+      end
+    end
+  end
+end
